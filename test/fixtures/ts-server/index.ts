9664a1d598abee5deec6d4f9469f46aee8d65a49
Rischerion <matthew.schnee@gmail.com>
2018-07-08 21:49:12 -0700

    working on more examples

--- test/fixtures/ts-server/index.ts
+++ test/fixtures/ts-server/index.ts
@@ -1,23 +1,43 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { toJs } from 'transit-immutable-js';
 
-import { Reducer, Store } from '../../../src';
+import { Store } from '../../../src';
 
-function getSomeState(req, res) {
-    return new Promise((resolve ,reject) => {
-        
+import IncDecReducer from './reducers/IncDecReducer';
+
+
+function setupState([req, res]) {
+    return new Promise((resolve) => {
+        res.locals = res.locals || {};
+        res.locals.store = new Store();
+        resolve([req, res]);
     });
 }
 
+function doSomeIncDec([req, res]) {
+    return new Promise((resolve) => {
+       res.locals.store.registerReducer(IncDecReducer);
+       res.locals.store.dispatch('INCREMENT');
+       resolve([req, res]);
+    });
+}
+
+function renderStateToString(state) {
+    return toJs(state);
+}
+
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`
-    <!doctype html>
-        <html>
-            <body>
-            </body>
-        </html>
-    `);
+    setupState([req, res]).then(doSomeIncDec).then(()=> {
+        res.setHeader('Content-Type', 'text/html');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`
+        <!doctype html>
+            <html>
+                <body>
+                </body>
+            </html>
+        `);
+    });
 });
 
 server.listen(8030, '127.0.0.1', () => {
